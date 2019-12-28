8962d923eb3b2237b125d85d548e8f81de373f85
LucianDavies <tldkhati@gmail.com>
2019-12-28 02:59:07 +0200

    rebuilding site Sat Dec 28 02:59:00 EET 2019

--- deploy.sh
+++ deploy.sh
@@ -18,12 +18,12 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin HEAD:master
+git push -f origin HEAD:master
 # Come Back up to the Project Root
 cd ..
 
 echo -e "\033[0;32mSyncronizing with source...\033[0m"
 git add .
 git commit -m "$msg"
-git push origin master
+git push -f origin master
 
